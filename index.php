e5460acaff2cfb543b1ec71db04d632dd57a84be
Ben Werdmuller <ben@benwerd.com>
2013-09-10 18:06:14 -0700

    Theme files!

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
         </title>
         <script src="//codeorigin.jquery.com/jquery-2.0.3.min.js" type="text/javascript"></script>
         <script src="//codeorigin.jquery.com/ui/1.10.3/jquery-ui.min.js" type="text/javascript"></script>
-        <link rel="stylesheet" type="text/css" href="style.css" />
+        <?php
+
+            if (file_exists('theme.inc')) {
+                include 'theme.inc';
+            } else {
+                ?><link rel="stylesheet" type="text/css" href="style.css" /><?php
+            }
+
+        ?>
     </head>
     <body>
         <div id="main_canvas">
